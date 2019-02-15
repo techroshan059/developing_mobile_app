5ab5c87e9e816ffd5f2091f98a94ecfa0ffe3f18
Roshan Shrestha <techroshan059@gmail.com>
2019-02-15 07:23:09 +0545

    second one;

--- week1/app/src/main/java/com/example/helloworld/MainActivity.java
+++ week1/app/src/main/java/com/example/helloworld/MainActivity.java
@@ -2,12 +2,49 @@ package com.example.helloworld;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    public Button btnToast,btnCount ;
+    public TextView txtCount;
+    public int number = 0 ;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnToast = (Button) findViewById(R.id.btnToast);
+        btnCount =(Button) findViewById(R.id.btnCount);
+        txtCount = (TextView) findViewById(R.id.txtCount);
+
+        btnToast.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Hello Toast", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btnCount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                number = number + 1;
+                if(number>100){
+                    number = 0 ;
+                    Toast.makeText(MainActivity.this, "Resetting Number to 0", Toast.LENGTH_SHORT).show();
+                }
+                txtCount.setText(Integer.toString(number));
+
+            }
+        });
+
     }
+
+
+
+
 }
